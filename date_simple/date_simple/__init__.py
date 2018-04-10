4ffa8026bf8865e464cc94691e04662d9dde651d
Allison E <allisone@Allisons-MBP.home>
2018-04-09 23:22:39 -0400

    fixed file

--- date_simple/date_simple/__init__.py
+++ date_simple/date_simple/__init__.py
@@ -6,48 +6,35 @@ Created on Mon Apr  9 21:45:34 2018
 @author: allisone
 """
 
-import date_simple as ds
 import datetime
-import pytest
 
-dateobj1 = ds.get_date_object()  
-testdate = datetime.date.today()
-dateobj2 = ds.get_date_object(date='2018-02-26')  
+def get_date_object(date=None):
+    if date == None:
+        x = datetime.date.today()
+    else:
+        if not isinstance(date, str):
+            raise ValueError('must be a string')
+        x = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+    return x
 
-datestr = ds.get_date_string()
-newstr = ds.get_date_string(date_object=dateobj2)
+def get_date_string(date_object=None, format='YYYY-mm-dd'):
+    format = format.lower()
+    for item in [['dd', '%d'], ['mm', '%m'], ['yyyy', '%Y'], ['yyy', '%y'], ['yy', '%Y'], ['mon', '%b']]:
+        format = format.replace(item[0], item[1])
+    if date_object == None:
+        date = datetime.date.today()
+else:
+    if not isinstance(date_object, datetime.date):
+        raise TypeError('must be in date format')
+        date = date_object
+    return date.strftime(format)
 
-datestr2 = ds.get_date_string(date_object=dateobj2, format='MM/DD/YYYY')
-datestr3 = ds.get_date_string(date_object=dateobj2, format='DD-Mon-YY')  # 26-Feb-18
+
+if __name__ == "__main__":
+    x = get_date_object()
+    print(type(get_date_object('2018-09-09')))
+    # print(get_date_string(get_date_object('01/01/2018'), format= "mm-dd-yyyy"))
+    print(get_date_string())
 
 
-def test_today():
-    assert dateobj1 == testdate
-
-def test_randdate():
-    assert dateobj2 == datetime.datetime.strptime('2018-02-26', '%Y-%m-%d').date()
-
-def testtoday2():
-    assert datestr == datetime.date.today().strftime('%Y-%m-%d')
-
-def test_randdate2():
-    assert newstr == dateobj2.strftime(format='%Y-%m-%d')
-
-def test_extra1():
-    assert datestr2 == '02/26/2018'
-    
-def test_extra2():
-    assert datestr3 == '26-Feb-2018'
-    
-def test_val1():
-    with pytest.raises(ValueError):
-        ds.get_date_object(date='bad date')
-        
-def test_type2():
-    with pytest.raises(TypeError):
-        ds.get_date_string(date_object='not_a_date_object')
-
-def test_type1():
-    with pytest.raises(ValueError):
-        ds.get_date_object(date=5)
         
